0317b524c5c977b19c452c00ab3dd213255b143b
Elijan Mastnak <git@ejmastnak.com>
2025-09-11 19:43:59 +0200

    refactor: create dedicated Publications partial

---
+++ src/Pages/Publications/Partials/Articles.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Article from '@/Components/Article.vue'
+
+const props = defineProps({
+  title: String,
+  // Array of objects with title, category, and href keys
+  articles: Array,
+})
+</script>
+
+<template>
+  <div>
+    <h3 class="text-2xl font-semibold">{{title}}</h3>
+    <ul class="mt-3 flex flex-col -mx-6 sm:mx-0 divide-y w-fit">
+      <li v-for="article in articles">
+        <Article :category="article.category" :title="article.title" />
+      </li>
+    </ul>
+  </div>
+</template>
