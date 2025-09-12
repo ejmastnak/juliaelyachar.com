dd25f3d78ee464deebb6ba4505ef5ed7cfd071f0
Elijan Mastnak <git@ejmastnak.com>
2025-09-12 15:11:39 +0200

    chore: add PageWrapper boilerplate to blank pages

--- src/Pages/Teaching.vue
+++ src/Pages/Teaching.vue
@@ -1,3 +1,12 @@
+<script setup>
+import PageWrapper from '@/Shared/PageWrapper.vue'
+</script>
+
 <template>
-  <h1 class="">Teaching</h1>
+  <PageWrapper>
+    <h1 class="text-5xl mb-5 lg:mb-8 pb-5 lg:pb-8 border-b border-gray-200">Teaching</h1>
+    <p class="italic">
+      Nothing here yet!
+    </p>
+  </PageWrapper>
 </template>
