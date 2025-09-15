9afde75eadbed5bb506f3c0c8b16b944ac190e9a
Elijan Mastnak <git@ejmastnak.com>
2025-09-15 16:42:41 +0200

    chore: remove third person from Bio

--- src/Pages/Home/Partials/Hero.vue
+++ src/Pages/Home/Partials/Hero.vue
@@ -16,13 +16,13 @@ import heroImg from '@/assets/img/hero.jpg?w=600'
       <img :src="heroImg" class="my-5 sm:h-72 rounded object-cover object-right" />
 
       <p class="max-w-lg">
-        I am an anthropologist broadly trained in economics, history of political and economic thought, political economy, social theory, Middle Eastern Studies, and Arabic language.
+        Julia Elyachar is an award-winning author, anthropologist, and political economist.
       </p>
       <p class="mt-3 max-w-lg">
-        I draw on my region-based training in Arabic language, Middle Eastern studies, and Ottoman history as well as disciplinary training in anthropology, economics, political economy, and history of economic and political thought to contribute to debates at the intersection of political economy, social theory, and anthropology.
+        She was trained in anthropology, economics, history of political and economic thought, political economy, social theory, Middle Eastern Studies, and Arabic language. At Princeton, she is an associate professor of anthropology, and associate professor at the Princeton Institute for International and Regional Studies. She is a Faculty Researcher with the Dignity and Debt network and serves on the Executive Boards of the Princeton Institute for International and Regional Studies, and the Center for Iran and Persian Gulf Studies. 
       </p>
         
-        <RouterLink to="/books/on-the-semicivilized" class="mt-6 inline-flex gap-x-1 items-center text-center rounded-full bg-orange-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"><p>See my new book: <span class="italic">On the Semicivilized</span></p><ChevronRightIcon class="size-5 shrink-0"/></RouterLink>
+        <RouterLink to="/books/on-the-semicivilized" class="mt-6 inline-flex gap-x-1 items-center text-center rounded-full bg-orange-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"><p>New book: <span class="italic">On the Semicivilized</span></p><ChevronRightIcon class="size-5 shrink-0"/></RouterLink>
 
     </div>
 
@@ -34,12 +34,12 @@ import heroImg from '@/assets/img/hero.jpg?w=600'
       <div class="max-w-md">
         <h1 class="text-4xl">Julia Elyachar</h1>
         <p class="mt-5">
-          I am an anthropologist broadly trained in economics, history of political and economic thought, political economy, social theory, Middle Eastern Studies, and Arabic language.
+          Julia Elyachar is an award-winning author, anthropologist, and political economist.
         </p>
         <p class="mt-3">
-          I draw on my region-based training in Arabic language, Middle Eastern studies, and Ottoman history as well as disciplinary training in anthropology, economics, political economy, and history of economic and political thought to contribute to debates at the intersection of political economy, social theory, and anthropology.
+          She was trained in anthropology, economics, history of political and economic thought, political economy, social theory, Middle Eastern Studies, and Arabic language. At Princeton, she is an associate professor of anthropology, and associate professor at the Princeton Institute for International and Regional Studies. She is a Faculty Researcher with the Dignity and Debt network and serves on the Executive Boards of the Princeton Institute for International and Regional Studies, and the Center for Iran and Persian Gulf Studies. 
         </p>
-        <RouterLink to="/books/on-the-semicivilized" class="mt-6 inline-flex gap-x-1 items-center text-center rounded-full bg-orange-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"><p>See my new book: <span class="italic">On the Semicivilized</span></p><ChevronRightIcon class="size-5 shrink-0"/></RouterLink>
+        <RouterLink to="/books/on-the-semicivilized" class="mt-6 inline-flex gap-x-1 items-center text-center rounded-full bg-orange-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"><p>New book: <span class="italic">On the Semicivilized</span></p><ChevronRightIcon class="size-5 shrink-0"/></RouterLink>
       </div>
 
     </div>
