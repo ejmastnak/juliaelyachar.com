1094a0e632cc3fce198d794b4d2b3e156546f552
Elijan Mastnak <git@ejmastnak.com>
2025-09-15 17:03:21 +0200

    refactor: replace Hero image with julia.jpg
    Replaces original hero image with julia.jpg, which Julia prefers.

--- src/Pages/Home/Partials/Hero.vue
+++ src/Pages/Home/Partials/Hero.vue
@@ -1,35 +1,37 @@
 <script setup>
 import { ChevronRightIcon } from '@heroicons/vue/24/outline'
-import heroImg from '@/assets/img/hero.jpg?w=600'
+import juliaImg from '@/assets/img/julia.jpg?w=600'
 </script>
 
 <template>
   <!-- Hero -->
   <div
-    class="px-5 sm:px-8 pt-8 sm:pt-12 lg:pt-24 pb-36 bg-sc-brown w-full text-sc-white clipped" 
+    class="px-5 sm:px-8 pt-8 sm:pt-12 md:pt-24 pb-36 bg-sc-brown w-full text-sc-white clipped" 
   >
 
     <!-- Mobile -->
-    <div class="lg:hidden mx-auto w-fit">
+    <div class="md:hidden mx-auto w-fit">
       <h1 class="text-4xl">Julia Elyachar</h1>
 
-      <img :src="heroImg" class="my-5 sm:h-72 rounded object-cover object-right" />
+      <img :src="juliaImg" class="my-5 mx-auto sm:mx-0 w-full max-w-[200px] sm:max-w-[300px] h-64 rounded object-cover object-right" />
 
-      <p class="max-w-lg">
-        Julia Elyachar is an award-winning author, anthropologist, and political economist.
-      </p>
-      <p class="mt-3 max-w-lg">
-        She was trained in anthropology, economics, history of political and economic thought, political economy, social theory, Middle Eastern Studies, and Arabic language. At Princeton, she is an associate professor of anthropology, and associate professor at the Princeton Institute for International and Regional Studies. She is a Faculty Researcher with the Dignity and Debt network and serves on the Executive Boards of the Princeton Institute for International and Regional Studies, and the Center for Iran and Persian Gulf Studies. 
-      </p>
-        
+      <div class="max-w-lg text-center sm:text-left">
+        <p>
+          Julia Elyachar is an award-winning author, anthropologist, and political economist.
+        </p>
+        <p class="mt-3">
+          She was trained in anthropology, economics, history of political and economic thought, political economy, social theory, Middle Eastern Studies, and Arabic language. At Princeton, she is an associate professor of anthropology, and associate professor at the Princeton Institute for International and Regional Studies. She is a Faculty Researcher with the Dignity and Debt network and serves on the Executive Boards of the Princeton Institute for International and Regional Studies, and the Center for Iran and Persian Gulf Studies. 
+        </p>
+
         <RouterLink to="/books/on-the-semicivilized" class="mt-6 inline-flex gap-x-1 items-center text-center rounded-full bg-orange-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"><p>New book: <span class="italic">On the Semicivilized</span></p><ChevronRightIcon class="size-5 shrink-0"/></RouterLink>
+      </div>
 
     </div>
 
     <!-- Desktop -->
-    <div class="hidden lg:flex max-w-6xl w-fit mx-auto gap-x-12 xl:gap-x-16">
+    <div class="hidden md:flex max-w-6xl w-fit mx-auto gap-x-12 xl:gap-x-16">
 
-      <img :src="heroImg" class="max-w-sm xl:max-w-md h-80 rounded object-cover object-right" />
+      <img :src="juliaImg" class="max-w-sm xl:max-w-md h-80 rounded object-cover object-right" />
 
       <div class="max-w-md">
         <h1 class="text-4xl">Julia Elyachar</h1>
