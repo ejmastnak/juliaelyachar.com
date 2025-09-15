e3107f0b34d90be1042c9685b84affdef5dc6e95
Elijan Mastnak <git@ejmastnak.com>
2025-09-15 17:04:03 +0200

    revert: remove image from Home/About section

--- src/Pages/Home/Partials/About.vue
+++ src/Pages/Home/Partials/About.vue
@@ -9,10 +9,9 @@ import juliaImg from '@/assets/img/julia.jpg?w=500'
     <!-- Mobile -->
     <div class="sm:hidden max-w-6xl w-fit mx-auto">
       <h2 class="text-3xl text-center">About Professor Elyachar</h2>
-      <img :src="juliaImg" class="my-5 sm:my-0 max-w-md h-72 rounded object-cover object-right-bottom mx-auto" />
       <div class="max-w-md text-sc-white">
         <p class="mt-5">
-          Julia Elyachar is a professor of anthropology at the Princeton University Department of Anthropology and the Princeton Institute for International and Regional Studies.
+          Julia Elyachar is associate professor of anthropology at the Princeton University Department of Anthropology and the Princeton Institute for International and Regional Studies.
           She is the author of the books <span class="italic">On the Semicivilized: Coloniality, Finance, and Embodied Sovereignty in Cairo</span> and <span class="italic">Markets of Dispossession: NGOs, Economic Development, and the State in Cairo</span>.
           Her work draws on fine-grained ethnography and regional expertise in the Middle East, Levant, and the Maghreb to open up areas for theoretical inquiry and conceptional innovation in anthropology and the social sciences more broadly.
         </p>
@@ -21,17 +20,16 @@ import juliaImg from '@/assets/img/julia.jpg?w=500'
     </div>
 
     <!-- Desktop -->
-    <div class="hidden sm:flex max-w-6xl w-fit mx-auto gap-x-10">
+    <div class="hidden sm:block max-w-6xl w-fit mx-auto gap-x-10">
       <div class="max-w-md text-sc-white">
         <h2 class="text-4xl">About Professor Elyachar</h2>
         <p class="mt-5">
-          Julia Elyachar is a professor of anthropology at the Princeton University Department of Anthropology and the Princeton Institute for International and Regional Studies.
+          Julia Elyachar is associate professor of anthropology at the Princeton University Department of Anthropology and the Princeton Institute for International and Regional Studies.
           She is the author of the books <span class="italic">On the Semicivilized: Coloniality, Finance, and Embodied Sovereignty in Cairo</span> and <span class="italic">Markets of Dispossession: NGOs, Economic Development, and the State in Cairo</span>.
           Her work draws on fine-grained ethnography and regional expertise in the Middle East, Levant, and the Maghreb to open up areas for theoretical inquiry and conceptional innovation in anthropology and the social sciences more broadly.
         </p>
         <RouterLink to="/about" class="mt-5 inline-block rounded-full bg-orange-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">More about Professor Elyachar</RouterLink>
       </div>
-      <img :src="juliaImg" class="max-w-md h-80 rounded object-cover object-right" />
     </div>
 
   </div>
